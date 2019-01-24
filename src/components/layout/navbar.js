612f593b0b3f092cbe907a1f7abac6803d77dedf
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-24 22:31:35 +0100

    show which is to show

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -3,21 +3,24 @@ import { Link } from 'react-router-dom'
 import SignedInLinks from './signedinlinks'
 import SignedOutLinks from './signedoutlinks'
 import { connect } from 'react-redux'
-const Navbar = () => {
+
+const Navbar = (props) => {
+  const { auth } = props;
+  // console.log(auth);
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
-        <Link to='/' className="brand-logo">Project Manager</Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        <Link to='/' className="brand-logo">MarioPlan</Link>
+        {links}
       </div>
     </nav>
   )
 }
 
-
 const mapStateToProps = (state) => {
-  console.log(state);
+  // console.log(state);
   return{
     auth: state.firebase.auth
   }
