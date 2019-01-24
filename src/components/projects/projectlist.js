a6d8b57165dd76858310bede3ef63060c8fc2ff6
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-24 17:28:50 +0100

    project summary using props

--- src/components/projects/projectlist.js
+++ src/components/projects/projectlist.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import ProjectSummary from './projectsummary'
 
-const ProjectList = () => {
+const ProjectList = ({projects}) => {
   return (
-    <div className="project-list section">  
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
+    <div className="project-list section">
+      { projects && projects.map(project => {
+        return (
+          <ProjectSummary project={project} key={project.id} />
+        )
+      })}  
     </div>
   )
 }
+
+
 
 export default ProjectList
