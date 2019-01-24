1a51395b95de9d0c77bf9a78d48bfeaa4b4db451
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-24 16:40:15 +0100

    edit routes

--- src/components/layout/signedoutlinks.js
+++ src/components/layout/signedoutlinks.js
@@ -6,8 +6,8 @@ const SignedOutLinks = () => {
   return (
     <div>
       <ul className="right">
-        <li><NavLink to='/'>Signup</NavLink></li>
-        <li><NavLink to='/'>Login</NavLink></li>
+      <li><NavLink to='/signup'>Signup</NavLink></li>
+        <li><NavLink to='/signin'>Login</NavLink></li>
       </ul>
     </div>
   )
