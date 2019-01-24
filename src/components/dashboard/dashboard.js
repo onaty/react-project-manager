7392998668365c3e81256f6a2921bf2578f59e50
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-24 20:57:50 +0100

    fetch live date

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import ProjectList from '../projects/projectlist'
 import Notifications from './notifications'
 import { connect } from 'react-redux'
-
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 class Dashboard extends Component {
   render() {
     
@@ -26,8 +27,13 @@ class Dashboard extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    projects: state.project.projects
+    projects: state.firestore.ordered.projects
   }
 }
 
-export default connect(mapStateToProps)(Dashboard)
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'projects' }
+  ])
+)(Dashboard)
