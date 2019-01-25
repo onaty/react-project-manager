b3e472761c6f5f3f13206c1cce9ffd97cc570c7a
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-25 03:11:37 +0100

    moving stuffs to dist folder

---
+++ dist/precache-manifest.b1e4227cf658f66eddb6953f44d40db6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "30ddd4dd72f886358332",
+    "url": "/static/js/main.30ddd4dd.chunk.js"
+  },
+  {
+    "revision": "0ea87f5c37444f1dc38c",
+    "url": "/static/js/1.0ea87f5c.chunk.js"
+  },
+  {
+    "revision": "30ddd4dd72f886358332",
+    "url": "/static/css/main.b78007d4.chunk.css"
+  },
+  {
+    "revision": "8383390aa116a02acfa999986552dbb2",
+    "url": "/index.html"
+  }
+];
